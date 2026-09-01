514e502ee3dfa861d658904be9457c1828e371b3
suprim-rijal <suprimrijal1@gmail.com>
2026-09-01 14:21:52 +0300

    feat(tours): add tour creation

--- tourHandlers.js
+++ tourHandlers.js
@@ -25,3 +25,23 @@ module.exports = {
   updateTour,
   deleteTour,
 };
+
+let tours = [];
+let nextId = 1;
+
+const addOne = (name, info, image, price, location) => {
+  const tour = {
+    id: nextId++,
+    name,
+    info,
+    image,
+    price,
+    location,
+  };
+  tours.push(tour);
+  return tour;
+};
+
+module.exports = {
+  addOne,
+};
