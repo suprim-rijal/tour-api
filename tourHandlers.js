d0d6621a636c10f84d34f57c00b42455a953f514
Prabhleen kaur <prabhleenkaur9946@gmail.com>
2026-09-01 14:46:05 +0300

    feat(api): implement create and retrieve all tours

--- tourHandlers.js
+++ tourHandlers.js
@@ -2,13 +2,15 @@
 const Tour = require("./tourLib");
 
 const getAllTours = (req, res) => {
-  res.json({ message: "Hello from getAllTours" });
+  const tours = Tour.getAll();
+  res.json(tours);
 };
 
 const createTour = (req, res) => {
-  res.json({ message: "Hello from createTour" });
+  const { name, info, image, price, location } = req.body;
+  const tour = Tour.addOne(name, info, image, price, location);
+  res.status(201).json(tour);
 };
-
 const getTourById = (req, res) => {
   res.json({ message: "Hello from getTourById" });
 };
